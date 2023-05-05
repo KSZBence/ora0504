3832d580005ddd56111179e828405aedb72e08a3
KSZBence <kunszekelybence@gmil.com>
2023-05-05 10:23:05 +0200

    ora 2. nem jo

--- Termekek.js
+++ Termekek.js
@@ -1,15 +1,23 @@
-import {ADATOK} from "./adatok.js";
 import Termek from "./Termek.js";
+import Aszinkron from "./aszinkron.js";
+
 class Termekek{
     #kedvncek = []
     constructor(){
-        let szuloElem = $("body main div")
-        for (let index = 0; index < ADATOK.length; index++) {
-            let termek = new Termek(ADATOK[index], szuloElem);
-        }
+        const aszinkron = new Aszinkron()
+        let vegpont = "adatok.json"
+        aszinkron.adatBeolvas(vegpont, this.#termekMegjelenito)
         $(window).on("gombEvent",(event)=>{
             this.#kedvncek.push(event.detail)
         })        
+    }
+    
+    #termekMegjelenito(lista){
+        let sajatlista = lista.LISTA
+        let szuloElem = $("body main div")
+        for (let index = 0; index < ADATOK.length; index++) {
+            let termek = new Termek(lista[index], szuloElem);
+        }
     }
 }
 
